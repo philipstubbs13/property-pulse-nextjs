c8714f75243f5e045e0e5ef7aef6f6baab82e092
Phil Stubbs <philipstubbs13@gmail.com>
2024-09-01 18:43:08 -0500

    nextjs fundamentals and project start

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
-import "@/assets/styles/globals.css";
+import Navbar from "../components/Navbar";
+import "../assets/styles/globals.css";
+import Footer from "../components/Footer";
 
 export const metadata = {
   title: "Property Pulse",
@@ -10,7 +12,9 @@ const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
+        <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,15 @@
+import HomeProperties from "../components/HomeProperties";
+import Hero from "../components/Hero";
+import InfoBoxes from "../components/InfoBoxes";
+
 const HomePage = () => {
-  return <div className="text-2xl">HomePage</div>;
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
+    </>
+  );
 };
 
 export default HomePage;
