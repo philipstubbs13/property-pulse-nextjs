d8d6673a289d287471b1121ab669ec155c39a02c
Phil Stubbs <philipstubbs13@gmail.com>
2024-09-02 11:54:01 -0500

    server actions and creating properties

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -4,6 +4,7 @@ import PropertyHeaderImage from "../../../components/PropertyHeaderImage";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 import PropertyDetails from "../../../components/PropertyDetails";
+import PropertyImages from "../../../components/PropertyImages";
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -29,6 +30,7 @@ const PropertyPage = async ({ params }) => {
           </div>
         </div>
       </section>
+      <PropertyImages images={property.images} />
     </>
   );
 };
