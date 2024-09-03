82f2649b617d29c18392c98d1532ead8c4914300
Phil Stubbs <philipstubbs13@gmail.com>
2024-09-03 18:33:14 -0500

    property search

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -6,6 +6,9 @@ import { FaArrowLeft } from "react-icons/fa";
 import PropertyDetails from "../../../components/PropertyDetails";
 import PropertyImages from "../../../components/PropertyImages";
 import { convertToSerializableObject } from "../../../utils/convertToObject";
+import BookmarkButton from "../../../components/BookmarkButton";
+import ShareButtons from "../../../components/ShareButtons";
+import PropertyContactForm from "../../../components/PropertyContactForm";
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -37,6 +40,11 @@ const PropertyPage = async ({ params }) => {
         <div class="container m-auto py-10 px-6">
           <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <PropertyDetails property={property} />
+            <aside className="space-y-4">
+              <BookmarkButton property={property} />
+              <ShareButtons property={property} />
+              <PropertyContactForm property={property} />
+            </aside>
           </div>
         </div>
       </section>
